18f3e737c1ed84f9a8ba1290b9a5a73e5788dd2d
Benedict <aliubenedict@gmail.com>
2023-12-09 22:57:33 +0100

    natours commit -2

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ app.get('/api/v1/tours', (req, res) => {
     },
   });
 });
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID',
+    });
+  }
+  const tour = tours.find((tour) => tour.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newID = tours[tours.length - 1].id + 1;
